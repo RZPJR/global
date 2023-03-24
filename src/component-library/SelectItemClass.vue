e0ab668feea55843c51c87c0134b3c8ead2ee180
Tebe <zaki.baihaki@edenfarm.id>
2023-03-24 15:57:12 +0700

    Select Item Class

--- src/component-library/SelectItemClass.vue
+++ src/component-library/SelectItemClass.vue
@@ -18,7 +18,7 @@
     >
         <template slot="selection" slot-scope="data">
             <div class="select-item">
-                {{ data.item.code }} - {{ data.item.description }}
+                {{ data.item.id }} - {{ data.item.name }}
             </div>
         </template>
         <template v-slot:label>
@@ -30,7 +30,7 @@
             </span>
         </template>
         <template slot="item" slot-scope="data">
-            {{ data.item.code }} - {{ data.item.description }}
+            {{ data.item.id }} - {{ data.item.name }}
         </template>
     </v-autocomplete>
 </template>
@@ -52,9 +52,9 @@
                 this.placeholder="Loading items..."
                 this.isLoading = true
                 // ini ke endpoint get all
-                this.$http.get("/bridge/v1/class",{params:{
-                    perpage:10,
-                    conditions:'status:1|name.icontains:'+search,
+                this.$http.get("/catalog/v1/item_class",{params:{
+                    page:1,
+                    per_page:10,
                 }}).then(response => {
                     this.items = response.data.data
                     if(this.items === null){
@@ -69,7 +69,7 @@
             },
             autoSelectByID(val) {
                 if(val){
-                    this.$http.get("/bridge/v1/class",{params:{
+                    this.$http.get("/catalog/v1/item_class",{params:{
                         conditions:'id.e:'+val.id,
                     }}).then(response => {
                         this.items.push(response.data.data[0])
